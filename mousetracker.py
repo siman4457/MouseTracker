e89eb467b54c8a0434e5c0486513717ad7e2f93e
Siman <siman4457@gmail.com>
2019-11-16 16:17:38 -0600

    initial commit

---
+++ mousetracker.py
@@ -0,0 +1,66 @@
+from pynput import mouse
+from pynput import keyboard
+
+
+def on_move(x, y):
+    print('Pointer moved to {0}'.format((x, y)))
+
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        print('Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
+    # print('{0} at {1}'.format('Pressed' if pressed else 'Released', (x, y)))
+    # if not pressed:
+    #     # Stop listener
+    #     return False
+
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0} at {1}'.format('down' if dy < 0 else 'up', (x, y)))
+
+
+#-------------------------------------
+# def on_press(key):
+#     print('{0} released'.format(key))
+#     if key == keyboard.Key.esc:
+#         # Stop listener
+#         return False
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+
+
+# Collect events until released
+# with mouse.Listener(
+#         on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+#     listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = mouse.Listener(
+    on_move=on_move, on_click=on_click, on_scroll=on_scroll)
+listener.start()
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+# listener = keyboard.Listener(
+#     on_press=on_press,
+#     on_release=on_release)
+# listener.start()
