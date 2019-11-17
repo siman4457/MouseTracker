cc9c88ff70b56ffb3e4faab860cc024031516e66
Siman <siman4457@gmail.com>
2019-11-16 20:07:35 -0600

    mousetracker2.py works. Capturing num keystrokes and num scrolls not working right now

--- mousetracker.py
+++ mousetracker.py
@@ -1,14 +1,45 @@
 from pynput import mouse
 from pynput import keyboard
+import csv
+import logging
+import time
+
+
+print("----------------------")
+print("     Mouse Tracker    ")
+print("----------------------")
+print("Hit the Esc key to stop recording")
+participant = input("Enter a name for your participant: ")
+print("Recording...")
+
+stopwatch = time.time()
+data = []
+
+
+#----------------File I/O---------------------
+def write_to_csv(data):
+    with open(participant + '_mouse_movement_.csv', mode='w') as movement_data_file:
+        movement_writer = csv.writer(movement_data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        movement_writer.writerow(['X','Y'])
+        for coordinate in data:
+            movement_writer.writerow(coordinate)
+
+# logging.basicConfig(filename=("mouse_log.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+# logging.basicConfig(filename=(participant + "mouse_clicks.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+# logging.basicConfig(filename=(participant + "keystrokes.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+
+
 
 #----------------Mouse Monitoring---------------------
 def on_move(x, y):
-    print('Pointer moved to {0}'.format((x, y)))
+    data.append([x, y])
+    # logging.info('Pointer moved to {0}'.format((x, y)))
 
 
 def on_click(x, y, button, pressed):
     if pressed:
-        print('Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
+        logging.info('Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
     # print('{0} at {1}'.format('Pressed' if pressed else 'Released', (x, y)))
     # if not pressed:
     #     # Stop listener
@@ -16,23 +47,25 @@ def on_click(x, y, button, pressed):
 
 
 def on_scroll(x, y, dx, dy):
-    print('Scrolled {0} at {1}'.format('down' if dy < 0 else 'up', (x, y)))
+    logging.info('Scrolled {0} at {1}'.format('down' if dy < 0 else 'up', (x, y)))
 
 
 #----------------Keyboard Monitoring---------------------
 
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
+        logging.info('alphanumeric key {0} pressed'.format(
             key.char))
     except AttributeError:
-        print('special key {0} pressed'.format(
+        logging.info('special key {0} pressed'.format(
             key))
 
 def on_release(key):
-    print('{0} released'.format(
+    logging.info('{0} released'.format(
         key))
     if key == keyboard.Key.esc:
+        write_to_csv(data)
+        print("Esc key pressed. Stopped recording.")
         # Stop listener
         return False
 
